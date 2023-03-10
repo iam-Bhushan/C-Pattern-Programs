37739c0701cf9e31dba2be688abad804c02515b7
iam-Bhushan <bhushanpatil.bdp@gmail.com>
2023-03-10 22:24:24 +0530

    27-30Pattern

---
+++ 27.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*
+void main()
+{
+    int i, j;
+    for (i = 'A'; i <= 'E'; i++)
+    {
+        for (j = 'A'; j <= 'E'; j++)
+        {
+            printf("%c ", j);
+        }
+        printf("\n");
+    }
+}
+*/
+void main()
+{
+    int i, j;
+    for (i = 65; i <= 69; i++)          // ASCII Value of A = 65
+    {
+        for (j = 65; j <= 69; j++)
+        {
+            printf("%c ", j);
+        }
+        printf("\n");
+    }
+}
+
---
+++ 29.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/*
+void main()
+{
+    int i,j;
+    for(i='E';i>='A';i--)
+    {
+        for(j='E';j>='A';j--)
+        {
+            printf("%c ",j);
+        }
+        printf("\n");
+    }
+}
+*/
+void main()
+{
+    int i,j;
+    for(i=69 ;i>=65; i--)
+    {
+        for(j=69;j>=65;j--)
+        {
+            printf("%c ",j);
+        }
+        printf("\n");
+    }
+}
