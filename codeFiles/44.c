12b924b06c222757ce77089001f5d39f7f41e91c
iam-Bhushan <bhushanpatil.bdp@gmail.com>
2023-03-11 19:39:04 +0530

    Modified

--- codeFiles/44.c
+++ codeFiles/44.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+/*
 void main()
 {
     int i, j, k, l = 1, m = 2;
@@ -15,3 +16,35 @@ void main()
         printf("\n");
     }
 }
+*/
+/*
+void main()
+{
+    int i, j, k=0,l;
+    for (i = 1; i <= 4; i++)
+    {
+        k += i;
+        l = k;
+        for (j = 1; j <= i; j++)
+        {
+            printf("%2d ", l);
+            l--;
+        }
+        printf("\n");
+    }
+}
+*/
+void main()
+{
+    int i, j, k=0;
+    for (i = 1; i <= 4; i++)
+    {
+        k += i;
+        for (j = k; j > (k - i) ; j--)
+        {
+            printf("%2d ", j);
+        }
+        printf("\n");
+    }
+}
+
