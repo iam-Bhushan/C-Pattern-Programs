ab4f3aa7ac398940607f333baf71741beaea125a
Bhushan Patil <bhushanpatil.bdp@gmail.com>
2023-03-13 20:30:41 +0530

    48th

---
+++ codeFiles/48.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    int i, j, k = 1,l=1,m;
+    for (i = 5; i >= 1; i--)
+    {
+        m = i;
+        for (j = 5; j >= i; j--)
+        {
+            printf("%d ", k);
+            k = k-m;
+            m++;
+        }
+        l += i;
+        k = l;
+        printf("\n");
+    }
+}
